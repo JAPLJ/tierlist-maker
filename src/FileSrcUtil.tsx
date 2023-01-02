dbbed755d543d3e632fb73d6c524127173efb465
JAPLJ <ys.qicos@gmail.com>
2023-01-02 20:29:13 +0900

    no longer needed

--- src/FileSrcUtil.tsx
+++ src/FileSrcUtil.tsx
@@ -1,10 +1,5 @@
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 
 export function fileSrc(path: string): string {
-  if (path.startsWith("/tmp")) {
-    return convertFileSrc(path);
-  } else {
-    // for testing purpose
-    return convertFileSrc(path);
-  }
+  return convertFileSrc(path);
 }
